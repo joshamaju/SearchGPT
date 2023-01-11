c00a28dd4d91352ec3403c5fce34ccc52d53d1db
Joshua Amaju <princejosh0007@gmail.com>
2023-01-11 21:19:01 +0100

    chore: add page progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,38 @@
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
 
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
 import "./style.css";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const query_client = new QueryClient();
+  const router = useRouter();
+
+  const [query_client] = useState(() => new QueryClient());
+
+  useEffect(() => {
+    const handleStart = (url: string) => {
+      console.log(`Loading: ${url}`);
+      NProgress.start();
+    };
+
+    const handleStop = () => {
+      NProgress.done();
+    };
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleStop);
+    router.events.on("routeChangeError", handleStop);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleStop);
+      router.events.off("routeChangeError", handleStop);
+    };
+  }, [router]);
 
   return (
     <QueryClientProvider client={query_client}>
